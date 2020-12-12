a4bace0bf0238b2782805bf873f9fb5b0b7d3c22
Ilia Beliaev <ilia.beliaev@t-systems.com>
2020-12-12 23:40:54 +0300

    Fixed @Sql.

--- src/test/java/com/silh/example/testcontainersdemo/repositories/ProductsRepositoryTest.java
+++ src/test/java/com/silh/example/testcontainersdemo/repositories/ProductsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.persistence.EntityManager;
@@ -16,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql("classpath:/data.sql")
 class ProductsRepositoryTest {
 
   @Autowired
--- src/test/java/com/silh/example/testcontainersdemo/repositories/UserRepositoryTest.java
+++ src/test/java/com/silh/example/testcontainersdemo/repositories/UserRepositoryTest.java
@@ -4,7 +4,9 @@ import com.silh.example.testcontainersdemo.entities.UserEntity;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -14,6 +16,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql("classpath:/data.sql")
 class UserRepositoryTest {
 
   @Autowired
